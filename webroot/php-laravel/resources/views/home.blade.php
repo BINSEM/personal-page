16b7e0d8b29f96c94f98f712e760d7c3c6588093
projetLaBonneRaquette <binsem125@gmail.com>
2018-11-29 22:29:26 +0100

    29112018

---
+++ webroot/php-laravel/resources/views/home.blade.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html >
+<head>
+  <meta charset="UTF-8">
+  <title>BINSEM</title>
+
+  <!-- Fonts -->
+  <link href="/css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
+
+</head>
+
+<body>
+  <div class="center">
+    <div id="social-test">
+      <h1>BINSEM</h1>
+      <ul class="social">
+
+        <li>
+          <a href="" alt="facebook">
+            <i class="fa fa-facebook" aria-hidden="true"></i>
+          </a>
+        </li>
+
+        <li>
+          <a href="" alt="twitter">
+            <i class="fa fa-twitter" aria-hidden="true"></i>
+          </a>
+        </li>
+        <li>
+          <a href="" alt="instagram">
+            <i class="fa fa-instagram" aria-hidden="true"></i>
+          </a>
+        </li>
+        <li>
+          <a href="" alt="vimeo">
+            <i class="fa fa-vimeo" aria-hidden="true"></i>
+          </a>
+        </li>
+        <li>
+          <a href="" alt="github">
+            <i class="fa fa-github" aria-hidden="true"></i>
+          </a>
+        </li>
+        <li>
+          <a href="" alt="cloud">
+            <i class="fa fa-cloud" aria-hidden="true"></i>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
+
+
+</body>
+</html>
